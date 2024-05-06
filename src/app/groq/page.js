366b1a69b1488bd27da837e474a71f997d31a392
rempra <remipradere@gmail.com>
2024-05-06 13:38:40 +0200

    howler test

--- src/app/groq/page.js
+++ src/app/groq/page.js
@@ -4,15 +4,21 @@ import { Howl } from 'howler';
 
 function Page() {
     const [text, setText] = useState('');
-    
+    const [audioReady, setAudioReady] = useState(false);
+
     const handleSpeak = async () => {
-        const apiKey = process.env.NEXT_PUBLIC_TEXT_SPEECH_GOOGLE; // Utilise la clé API stockée en variable d'environnement
+        if (!audioReady) {
+            console.log('User interaction required to play audio.');
+            return;
+        }
+
+        const apiKey = process.env.NEXT_PUBLIC_TEXT_SPEECH_GOOGLE;
         const postData = {
-            input: { text: text },   
-            voice: { languageCode: 'fr-FR', ssmlGender: 'NEUTRAL' }, // Changé en français
+            input: { text: text },
+            voice: { languageCode: 'fr-FR', ssmlGender: 'NEUTRAL' },
             audioConfig: { audioEncoding: 'MP3' },
         };
-        console.log(apiKey)
+
         const response = await fetch(`https://texttospeech.googleapis.com/v1/text:synthesize?key=${apiKey}`, {
             method: 'POST',
             headers: {
@@ -23,9 +29,7 @@ function Page() {
 
         const data = await response.json();
         if (data.audioContent) {
-            // Directement utiliser base64 pour créer une source audio pour Howler
             const audioSrc = `data:audio/mp3;base64,${data.audioContent}`;
-            console.log(audioSrc)
             const sound = new Howl({
                 src: [audioSrc],
                 format: ['mp3']
@@ -36,10 +40,15 @@ function Page() {
         }
     };
 
+    const enableAudio = () => {
+        setAudioReady(true);
+    };
+
     return (
         <div>
+            <button onClick={enableAudio}>Enable Audio</button>
             <input className='border mt-[100px]' type="text" value={text} onChange={e => setText(e.target.value)} />
-            <button onClick={handleSpeak}>Speak</button>
+            <button onClick={handleSpeak} disabled={!audioReady}>Speak</button>
         </div>
     );
 }
